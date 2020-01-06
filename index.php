0285cb6a35e37a42a9156b9cbf65d6ecc3fa022d
kossi <kossidanikey@yahoo.fr>
2020-01-06 15:08:58 +0100

    First depot front-end

--- index.php
+++ index.php
@@ -8,62 +8,14 @@
     <title></title>
   </head>
   <body>
-    <header>
-      <div class="row nopadding">
-        <div class="col-3">
-          <div class="logo">
+      <?php include("includes/header.php") ?>
 
-            <img src="" alt="">
-          </div>
-        </div>
-        <div class="col-9">
-          <div class="menu">
-            <nav>
-              <ul>
-                <li><a href="#">Accueil</a></li>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">Membres</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div>
-
-
-    </header>
     <div class="d-flex">
 
-    <aside>
-      <div class="row" style="margin-bottom:5px;">
-        <div class="col-6">
-          <p>Se connecter</p>
-        </div>
-        <div class="col-6 text-right">
-          <a class="register" href="register.php">S'inscrire</a>
-        </div>
-      </div>
-      <form class="" method="post">
-        <input type="text" class="inputlogin" name="pseudo" placeholder="Pseudo ou Mail">
-        <input type="password" class="inputlogin" name="motdepass" placeholder="Mot de passe">
-        <div class="row">
-          <div class="col-9 text-right">
-            <input type="submit" class="inputsubmit" name="connexion" value="Se connecter">
-          </div>
-
-        </div>
-
-      </form>
-      <div class="blocgauche">
-        <h4>Titre de la categorie</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <img src="" width="100%" alt="">
-      </div>
-    </aside>
+      <?php include("includes/sidebar.php") ?>
     <main>
       <div class="blocaccueil">
         <div class="row">
-
           <div class="col-8">
             <h1>Bienvenue sur le forum</h1>
             <p>Lorem ipsum d'index.php'
@@ -80,14 +32,12 @@ olor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
           <div style="padding: 10px;">
               <p>Titre de la categorie</p>
 
-
           </div>
-
         </div>
 
           <div class="row nopadding">
 
-          <a href="#">
+          <a href="sub-categories.php">
           <div class="col-12 blocsouscat">
             <p class="tittresouscat"> Titre de la sous categorie</p>
             <p class="font-13">Lorem ipsum dolor sit amet, labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
@@ -106,7 +56,7 @@ olor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
       </div>
     </main>
         </div>
-    <footer> test </footer>
+        <?php include("includes/footer.php") ?>
 
   </body>
 </html>
---
+++ register.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <?php include("includes/header.php") ?>
+
+    <div class="d-flex">
+
+      <?php include("includes/sidebar.php") ?>
+    <main>
+      <div class="blocaccueil">
+        <div class="row">
+
+          <div class="col-8">
+            <h1>Bienvenue sur le forum</h1>
+            <p>Lorem ipsum d'index.php'
+olor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+          </div>
+            <div class="col-4">
+              <img src="" alt="">
+              </div>
+      </div>
+    </div>
+      <div class="blocategorie">
+        <div class="titrecat">
+          <div style="padding: 10px;">
+              <p>Inscription</p>
+
+
+          </div>
+
+        </div>
+        <div class="row align-items-center">
+          <div class="col-8 ml-auto pt-20">
+                    <form class="" action="index.html" method="post">
+
+
+                    <input type="text" class="inputlogin" name="pseudo" placeholder="Pseudo">
+                    <input type="mail" class="inputlogin" name="mail" placeholder="Votre adresse e-mail">
+                    <input type="password" class="inputlogin" name="password" placeholder="Votre mot de passe">
+
+                        </form>
+          </div>
+          <div class="col-2">
+
+          </div>
+
+        </div>  <div class="d-flex justify-content-center">
+            <input type="submit" class="inputsubmit2" name="register" value="S'inscrire gratuitement">
+
+        </div>
+
+
+      </div>
+    </main>
+        </div>
+<?php include("includes/footer.php") ?>
+  </body>
+</html>
