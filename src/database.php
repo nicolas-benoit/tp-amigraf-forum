edf845dd977aee9582d48073359dac835004fcda
Nicolas Benoit <nicolasbenoit59390@gmail.com>
2020-01-06 12:11:55 +0100

    - Remove constreints from the db_template.sql. - Adding basic database system. - Add .gitignore for database environement.

---
+++ src/database.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once "databaseEnv.local.php";
+
+$db;
+
+function dbInit() {
+    global $db;
+    $db = new PDO("mysql:host=" . DB_HOST . ";port=3306;dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+}
+
+function dbExecute($sql, $args = []) {
+    global $db;
+    $query = $db->prepare($sql);
+    $query->execute($args);
+    return $query->fetchAll();
+}
