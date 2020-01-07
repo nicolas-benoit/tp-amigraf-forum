2fdec7675a2dea1daa58d064c0e068374aa6e381
Nicolas Benoit <nicolasbenoit59390@gmail.com>
2020-01-07 10:58:28 +0100

    Adding comments management functions for the database
    - Adding a comment
    - Get a comment
    - Get a list of comment from a topic id
    - Edit a comment
    - Forget a comment

--- index.php
+++ index.php
@@ -2,12 +2,10 @@
     include_once "src/user.php";
     include_once "src/category.php";
     include_once "src/subcategory.php";
-
     include_once "src/topic.php";
+    include_once "src/comment.php";
 
-    //pushUser(createUser("nicolas", "test", "dsfds", "Admin", date("Y-m-d H:i:s")));
-
-    var_dump(pullTopicList());
+    //$comment = createComment("This is a comment", date("Y-m-d H:i:s"));
 ?>
 
 <!DOCTYPE html>
--- src/comment.php
+++ src/comment.php
@@ -1,13 +1,77 @@
 <?php
 
-function createTopic($content, $date, $is_deleted = 0) {
+include_once "database.php";
+
+function createComment($content, $date, $is_deleted = 0) {
     return [
         "id" => -1,
-        "content" => $content
+        "content" => $content,
         "date" => $date,
         "is_deleted" => $is_deleted,
 
         "topic_id" => -1,
         "user_id" => -1
     ];
+}
+
+function pushComment($comment) {
+    dbInit();
+    $sql = "INSERT INTO comments(content,date,topic_id,user_id) VALUES (:content,:date,:topic_id,:user_id)";
+
+    dbExecute($sql, [
+        [":content", $comment["content"], PDO::PARAM_STR],
+        [":date", $comment["date"], PDO::PARAM_STR],
+        [":topic_id", $comment["topic_id"], PDO::PARAM_STR],
+        [":user_id", $comment["user_id"], PDO::PARAM_STR]
+    ]);
+}
+
+function pullComment($id) {
+    dbInit();
+    $comment = dbExecute("SELECT * FROM comments WHERE id=:id;", [[":id", $id, PDO::PARAM_INT]]);
+    return $comment[0];
+}
+
+function pullCommentList($topic_id, $amount = 20, $offset = 0) {
+    dbinit();
+    $sql = "SELECT * FROM comments WHERE topic_id=:topic_id AND is_deleted=0 ORDER BY date LIMIT :amount OFFSET :offset;";
+
+    $commentList = dbExecute($sql, [
+        [":topic_id", $topic_id, PDO::PARAM_INT],
+        [":amount", $amount, PDO::PARAM_INT],
+        [":offset", $offset, PDO::PARAM_INT]
+    ]);
+
+    return $commentList;
+}
+
+function updateComment($comment) {
+    dbinit();
+    $sql = "UPDATE comments SET content = :content, date = :date,topic_id = :topic_id,user_id = :user_id WHERE id=:id;";
+
+    dbExecute($sql, [
+        [":id", $comment["id"], PDO::PARAM_INT],
+        [":content", $comment["content"], PDO::PARAM_STR],
+        [":date", $comment["date"], PDO::PARAM_STR],
+        [":topic_id", $comment["topic_id"], PDO::PARAM_STR],
+        [":user_id", $comment["user_id"], PDO::PARAM_STR]
+    ]);
+}
+
+function forgetComment($comment) {
+    dbInit();
+    $sql = "UPDATE comments SET is_deleted=1 WHERE id=:id";
+    dbExecute($sql, [
+        [":id", $comment["id"], PDO::PARAM_INT]
+    ]);
+}
+
+function linkTopicToComment($comment, $topic) {
+    $comment["topic_id"] = $topic["id"];
+    return $comment;
+}
+
+function linkUserToComment($comment, $user) {
+    $comment["user_id"] = $user["id"];
+    return $comment;
 }
